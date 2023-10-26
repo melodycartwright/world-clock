57a21477932b3de462872554eed1ba41574c21ff
melodycartwright <melody94c@gmail.com>
2023-10-26 17:57:17 +0200

    added timezone to homepage

--- index.js
+++ index.js
@@ -24,6 +24,15 @@ function updateTime() {
       "h:mm:ss [<small>]A[</small>]"
     );
   }
+    let tokyoElement = document.querySelector("#tokyo");
+  if (tokyoElement) {
+    let tokyoDateElement = tokyoElement.querySelector(".date");
+    let tokyoTimeElement = tokyoElement.querySelector(".time");
+    let tokyoTime = moment().tz("Asia/Tokyo");
+
+    tokyoDateElement.innerHTML = tokyoTime.format("MMMM	Do YYYY");
+    tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
+  }
 }
  function updateCity(event) {
   let cityTimeZone = event.target.value;
